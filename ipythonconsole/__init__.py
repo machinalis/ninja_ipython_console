80654ab20694207c3066455213466c899af56d4c
Horacio Duran <hduran@machinalis.com>
2013-10-17 13:16:49 -0300

    Added compatibility for 1.x.x

--- ipythonconsole/__init__.py
+++ ipythonconsole/__init__.py
@@ -1,1 +1,5 @@
-from ipythonconsole import INinjaConsole
+try:
+    from IPython.zmq.ipkernel import IPKernelApp
+    from ipythonconsole import INinjaConsole
+except ImportError:
+    from ipythonconsole_1_1_x_compat import INinjaConsole
--- ipythonconsole/ipythonconsole_1_1_x_compat.py
+++ ipythonconsole/ipythonconsole_1_1_x_compat.py
@@ -23,14 +23,9 @@ from ninja_ide.core import plugin
 
 import atexit
 
-try:
-    from IPython.zmq.ipkernel import IPKernelApp
-except ImportError:
-    from IPython.kernel.zmq.kernelapp import IPKernelApp
-from IPython.lib.kernel import find_connection_file
-from IPython.frontend.qt.kernelmanager import QtKernelManager
-from IPython.frontend.qt.console.rich_ipython_widget import RichIPythonWidget
-from IPython.utils.traitlets import TraitError
+from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+from IPython.qt.inprocess import QtInProcessKernelManager
+
 from PyQt4 import QtCore
 from PyQt4.QtCore import SIGNAL
 
@@ -39,62 +34,33 @@ from ninja_ide.resources import IMAGES
 from PyQt4.QtGui import QWidget
 
 
-class IPythonWidget(QWidget, itab_item.ITabItem):
-
-    def __init__(self):
-        super(IPythonWidget, self).__init__()
-
-
-def event_loop(kernel):
-    kernel.timer = QtCore.QTimer()
-    kernel.timer.timeout.connect(kernel.do_one_iteration)
-    kernel.timer.start(1000 * kernel._poll_interval)
-
-
-def default_kernel_app():
-    app = IPKernelApp.instance()
-    app.initialize(['python', '--pylab=qt'])
-    app.kernel.eventloop = event_loop
-    return app
-
-
-def default_manager(kernel):
-    connection_file = find_connection_file(kernel.connection_file)
-    manager = QtKernelManager(connection_file=connection_file)
-    manager.load_connection_file()
-    manager.start_channels()
-    atexit.register(manager.cleanup_connection_file)
-    return manager
-
-
-def console_widget(manager):
-    try:  # Ipython v0.13
-        widget = RichIPythonWidget(gui_completion='droplist')
-    except TraitError:  # IPython v0.12
-        widget = RichIPythonWidget(gui_completion=True)
-    widget.kernel_manager = manager
-    return widget
-
-
-def terminal_widget(**kwargs):
-    kernel_app = default_kernel_app()
-    manager = default_manager(kernel_app)
-    widget = console_widget(manager)
-
-    #update namespace
-    kernel_app.shell.user_ns.update(kwargs)
-
-    kernel_app.start()
-    return widget
-
-
 class INinjaConsole(plugin.Plugin):
     def initialize(self):
         # Init your plugin
         self.misc_s = self.locator.get_service('misc')
         explorer_container = self.locator.get_service('explorer')
         tree = explorer_container.get_tree_projects()
-        self.ipython_console = terminal_widget()
+
+
+        kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel()
+        kernel = kernel_manager.kernel
+        kernel.gui = 'qt4'
+
+        kernel_client = kernel_manager.client()
+        kernel_client.start_channels()
+
+        def stop():
+            kernel_client.stop_channels()
+            kernel_manager.shutdown_kernel()
+
+        self.ipython_console = RichIPythonWidget()
+        self.ipython_console.kernel_manager = kernel_manager
+        self.ipython_console.kernel_client = kernel_client
+        self.ipython_console.exit_requested.connect(stop)
+        self.ipython_console.show()
+
+
         self.misc_s.add_widget(self.ipython_console, IMAGES["console"],
                                 "IPython console")
         addp = SIGNAL("addProjectToConsole(QString)")
